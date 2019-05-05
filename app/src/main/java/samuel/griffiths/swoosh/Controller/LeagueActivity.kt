d2ac5b5f8ff205022243ec38f1a2a8b3ac32745f
pearjam <samgriffiths7@gmail.com>
2019-05-05 20:23:47 +0200

    end of final swoosh lesson (10). ic_laucher icons didin't work right. not transparent background, and old icons weren't replaced in directory...

--- app/src/main/java/samuel/griffiths/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/samuel/griffiths/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package samuel.griffiths.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
@@ -13,9 +14,21 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("","")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onMensClicked(view: View){
